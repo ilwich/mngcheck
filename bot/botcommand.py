5991a3dc389dcc49f8dbcfc1fbe456e5b92b83bb
ilwich <ilwich@mail.ru>
2022-01-11 22:44:55 +0300

    profile and qr payment adding3

--- bot/botcommand.py
+++ bot/botcommand.py
@@ -11,10 +11,12 @@ from re import findall, compile, fullmatch
 
 def fn_number_in_msg(str_in):
     """Выделим номер ФН в тексте сообщений"""
-    for words in str_in.split():
-        fn_number = ''.join(i for i in words if i.isdigit())
-        if len(fn_number) == 16: return fn_number
-    return False
+    pattern = '(\d{16})'
+    res = findall(pattern, str_in)
+    if res:
+        return res[0]
+    else:
+        return False
 
 
 def summ_in_msg(str_in):
