6b458c4c06f9e0d7d08e6b14c92ea34542280cb3
ilwich <ilwich@mail.ru>
2022-02-24 22:15:13 +0300

    whitenoise add4

--- mngcheck/settings.py
+++ mngcheck/settings.py
@@ -72,7 +72,7 @@ if os.environ.get("IN_SERVER"):
             'file': {
                 'level': 'INFO',
                 'class': 'logging.handlers.RotatingFileHandler',
-                'filename': os.path.join(MEDIA_URL, 'djangolog.txt'),
+                'filename': os.path.join('/app/log/', 'djangolog.txt'),
                 'maxBytes': 1024*1024*15, # 15MB
                 'backupCount': 10,
             },
